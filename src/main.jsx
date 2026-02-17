876b6c0cb4b631b2d3a923a5710483960ab0beed
Samin Ahmed <saminahmed1001@gmail.com>
2026-02-18 03:25:24 +0600

    Feat: add observser for description to render

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function main() {
+  const container = document.createElement("div");
+  container.id = "youLyricRoot";
+  const descriptionDiv = document.querySelector("#description-inner");
+  descriptionDiv.insertBefore(container, descriptionDiv.firstChild);
+
+  createRoot(container).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}
+
+const observer = new MutationObserver(() => {
+  if (
+    document.querySelector("#description-inner") &&
+    document.querySelector(".yt-video-attribute-view-model__metadata")
+  ) {
+    observer.disconnect();
+
+    main();
+  }
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
