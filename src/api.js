43c5518189c28b110bcb4479fe61984c8f87688f
Samin Ahmed <saminahmed1001@gmail.com>
2026-02-21 00:06:33 +0600

    Feat: function that fetches the lyric

---
+++ src/api.js
@@ -0,0 +1,20 @@
+export default async function fetchLyrics(title, author) {
+  const updatedTitle = encodeURIComponent(title);
+  const updatedAuthor = encodeURIComponent(author);
+
+  const url = `https://lrclib.net/api/search?track_name=${updatedTitle}&artist_name=${updatedAuthor}`;
+
+  try {
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    if (Object.keys(data).length <= 0) {
+      return 404;
+    }
+
+    return data;
+  } catch (error) {
+    return `Error: ${error}`;
+  }
+}
