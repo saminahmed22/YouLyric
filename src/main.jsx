9a7830590deb1a212269f2e9a2786ca679117fdb
Samin Ahmed <saminahmed1001@gmail.com>
2026-02-21 00:08:40 +0600

    Feat: various featrues for manual searching and youtube navigation

--- src/main.jsx
+++ src/main.jsx
@@ -3,31 +3,101 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-function main() {
+let observer = null;
+
+let title, author;
+
+// bad solution for manual search but I'll see it later
+export function main(title, author, method = "auto") {
+  if (method !== "auto") {
+    const container = document.querySelector("#youLyricRoot");
+    if (container) {
+      container.remove();
+    }
+  }
   const container = document.createElement("div");
   container.id = "youLyricRoot";
-  const descriptionDiv = document.querySelector("#description-inner");
-  descriptionDiv.insertBefore(container, descriptionDiv.firstChild);
+
+  const middleRowDiv = document.querySelector("#middle-row");
+  middleRowDiv.appendChild(container);
 
   createRoot(container).render(
     <StrictMode>
-      <App />
+      <App title={title} author={author} />
     </StrictMode>,
   );
 }
 
-const observer = new MutationObserver(() => {
-  if (
-    document.querySelector("#description-inner") &&
-    document.querySelector(".yt-video-attribute-view-model__metadata")
-  ) {
-    observer.disconnect();
-
-    main();
+document.addEventListener("yt-navigate-start", () => {
+  const lyricDiv = document.querySelector("#youLyricRoot");
+  if (lyricDiv) {
+    lyricDiv.remove();
+    if (observer) {
+      observer.disconnect();
+      observer = null;
+    }
   }
 });
 
-observer.observe(document.body, {
-  childList: true,
-  subtree: true,
+document.addEventListener("yt-navigate-finish", () => {
+  observer = new MutationObserver(() => {
+    const middleRowDiv = document.querySelector("#middle-row");
+    const metadata = document.querySelector(
+      ".yt-video-attribute-view-model__metadata",
+    );
+    if (middleRowDiv && metadata) {
+      const newTitle = document.querySelector(
+        ".yt-video-attribute-view-model__title",
+      ).textContent;
+      const newAuthor = document.querySelector(
+        ".yt-video-attribute-view-model__subtitle",
+      ).textContent;
+
+      if (newTitle === title || newAuthor === author) {
+        return;
+      }
+
+      title = newTitle;
+      author = newAuthor;
+      observer.disconnect();
+      main(title, author);
+    }
+  });
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+  });
 });
+
+//issue playing certain songs, best guess, playlists caches the videos, and might trigger different singals
+// document.addEventListener("yt-page-data-updated", () => {
+//   observer = new MutationObserver(() => {
+//     const middleRowDiv = document.querySelector("#middle-row");
+//     const metadata = document.querySelector(
+//       ".yt-video-attribute-view-model__metadata",
+//     );
+//     if (middleRowDiv && metadata) {
+//       const newTitle = document.querySelector(
+//         ".yt-video-attribute-view-model__title",
+//       ).textContent;
+//       const newAuthor = document.querySelector(
+//         ".yt-video-attribute-view-model__subtitle",
+//       ).textContent;
+
+//       if (newTitle === title || newAuthor === author) {
+//         return;
+//       }
+
+//       title = newTitle;
+//       author = newAuthor;
+//       observer.disconnect();
+//       main(title, author);
+//     }
+//   });
+
+//   observer.observe(document.body, {
+//     childList: true,
+//     subtree: true,
+//   });
+// });
