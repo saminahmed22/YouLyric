6b862f181c806d9091bebe37c87e7d4f0375f57e
Samin Ahmed <saminahmed1001@gmail.com>
2026-02-21 00:09:52 +0600

    Feat: loading page and manual input form

--- src/Components/LoadingScreen/LoadingScreen.jsx
+++ src/Components/LoadingScreen/LoadingScreen.jsx
@@ -1,12 +1,53 @@
+import { useContext, useEffect } from "react";
 import styles from "./LoadingScreen.module.css";
+
+import { AppContext } from "../../context";
+import fetchLyrics from "../../api";
 
 import loadingIcon from "../../../icons/loading.png";
 
-export default function LoadingScreen() {
-  return (
-    <div className={styles.LoadingScreenDiv}>
-      <img className={styles.loadingIcon} src={loadingIcon} alt="" />
-      <p>Loading</p>
-    </div>
-  );
+import ManualSearchForm from "./ManualSearchForm/ManualSearchForm";
+
+export default function LoadingScreen({ title, author }) {
+  const { loaded, setLoaded, setLyrics } = useContext(AppContext);
+
+  useEffect(() => {
+    const getLyric = async () => {
+      const response = await fetchLyrics(title, author);
+      if (response === 404) {
+        setLoaded(404);
+      } else {
+        setLyrics(response);
+        setLoaded(true);
+      }
+    };
+
+    getLyric();
+  }, [title, author, setLyrics, setLoaded]);
+
+  const loadingScreenContent = () => {
+    if (loaded === 404) {
+      return (
+        <div className={styles.cantFindDiv}>
+          <ManualSearchForm />
+          <p>{`Unable to find lyrics :(,
+          this mainly happens because of 'junk' in the name of the song or the author.
+          Please search for the lyrics manually`}</p>
+        </div>
+      );
+    } else {
+      return (
+        <div className={styles.LoadingScreenDiv}>
+          <img
+            className={styles.loadingIcon}
+            src={loadingIcon}
+            alt="Loading icon"
+          />
+          <p>Fetching the lyrics</p>
+        </div>
+      );
+    }
+  };
+
+  return loadingScreenContent();
 }
