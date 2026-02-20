555838781e50d5da09eb4368f4518eaefa504274
Samin Ahmed <saminahmed1001@gmail.com>
2026-02-20 16:01:06 +0600

    Feat: add scrolling feature for lyrics

--- src/Components/Lyric/Lyric.jsx
+++ src/Components/Lyric/Lyric.jsx
@@ -1,57 +1,61 @@
+import { useContext, useState } from "react";
+import { AppContext } from "../../context";
+import ActDiv from "../ActDiv/ActDiv";
+
 import styles from "./Lyric.module.css";
 
+import rightIcon from "../../../icons/chevronRight.png";
+import leftIcon from "../../../icons/chevronLeft.png";
+
 export default function Lyric() {
+  const { lyrics, fontSize } = useContext(AppContext);
+  const [currentLyricIndex, setCurrentLyric] = useState(0);
+  const lyricsQuantity = lyrics.length;
+  const currentLyrics = lyrics[currentLyricIndex].plainLyrics;
+
   return (
     <div className={styles.contentDiv}>
-      <div className={styles.lyricDivHeader}>Lyrics</div>
-      <pre className={styles.lyricText}>
-        [Verse 1]
-        {`Remember that night
-White steps in the moonlight
-They walked here too
-Through empty playground, this ghost's town
-Children again
-On rusting swings getting higher
-Sharing a dream
-On an island, it felt right
-
-[Pre-Chorus]
-We lay side by side
-Between the moon and the tide
-Mapping the stars for a while
-
-[Chorus]
-Let the night surround you
-We're halfway to the stars
-Ebb and flow, let it go
-Feel her warmth beside you
-
-[Verse 2]
-Remember that night
-The warmth and the laughter
-Candles burned
-Though the church was deserted
-At dawn we went down
-Through empty streets to the harbour
-Dreamers may leave
-But they're here ever after
-(Da, da, da, da, da)
-(Da, da, da, da, da)
-(Da, da, da, da, da)
-(Da)
-You might also like
-Where We Start
-David Gilmour
-This Heaven
-David Gilmour
-Smile
-David Gilmour
-[Chorus]
-Let the night surround you
-We're halfway to the stars
-Ebb and flow, let it go
-Feel her warmth beside you`}
-      </pre>
+      <div className={styles.contentHeader}>
+        <div className={styles.lyricDivHeader}>
+          Lyrics
+          <div className={styles.lyricScroll}>
+            <button
+              className={styles.scrollBtn}
+              title="Scroll left"
+              onClick={() => {
+                setCurrentLyric(currentLyricIndex - 1);
+              }}
+              disabled={currentLyricIndex + 1 <= 1}
+            >
+              <img src={leftIcon} alt="Left icon" />
+            </button>
+            <span>
+              {currentLyricIndex + 1}/{lyricsQuantity}
+            </span>
+            <button
+              className={styles.scrollBtn}
+              title="Scroll right"
+              onClick={() => {
+                setCurrentLyric(currentLyricIndex + 1);
+              }}
+              disabled={currentLyricIndex + 1 >= lyricsQuantity}
+            >
+              <img src={rightIcon} alt="Right icon" />
+            </button>
+          </div>
+        </div>
+        <ActDiv />
+      </div>
+      <div className={styles.lyricContainer}>
+        <pre
+          className={styles.lyricText}
+          style={{
+            fontSize: `${fontSize}rem`,
+          }}
+        >
+          {currentLyrics}
+        </pre>
+      </div>
     </div>
   );
 }
