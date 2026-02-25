a8563991d88b3f042a09e77d42a949a168421c3c
Samin Ahmed <saminahmed1001@gmail.com>
2026-02-26 05:12:51 +0600

    Refactor: remove header div to dedicated component

--- src/Components/Lyric/Lyric.jsx
+++ src/Components/Lyric/Lyric.jsx
@@ -1,51 +1,16 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { AppContext } from "../../context";
-import ActDiv from "../ActDiv/ActDiv";
 
 import styles from "./Lyric.module.css";
 
-import rightIcon from "../../../icons/chevronRight.png";
-import leftIcon from "../../../icons/chevronLeft.png";
-
 export default function Lyric() {
   const { lyrics, fontSize } = useContext(AppContext);
-  const [currentLyricIndex, setCurrentLyric] = useState(0);
-  const lyricsQuantity = lyrics.length;
-  const currentLyrics = lyrics[currentLyricIndex].plainLyrics;
+
+  const currentLyricsIndex = lyrics.currentlySelectedLyrics;
+  const currentLyrics = lyrics.fetchedLyrics[currentLyricsIndex].plainLyrics;
 
   return (
     <div className={styles.contentDiv}>
-      <div className={styles.contentHeader}>
-        <div className={styles.lyricDivHeader}>
-          Lyrics
-          <div className={styles.lyricScroll}>
-            <button
-              className={styles.scrollBtn}
-              title="Scroll left"
-              onClick={() => {
-                setCurrentLyric(currentLyricIndex - 1);
-              }}
-              disabled={currentLyricIndex + 1 <= 1}
-            >
-              <img src={leftIcon} alt="Left icon" />
-            </button>
-            <span>
-              {currentLyricIndex + 1}/{lyricsQuantity}
-            </span>
-            <button
-              className={styles.scrollBtn}
-              title="Scroll right"
-              onClick={() => {
-                setCurrentLyric(currentLyricIndex + 1);
-              }}
-              disabled={currentLyricIndex + 1 >= lyricsQuantity}
-            >
-              <img src={rightIcon} alt="Right icon" />
-            </button>
-          </div>
-        </div>
-        <ActDiv />
-      </div>
       <div className={styles.lyricContainer}>
         <pre
           className={styles.lyricText}
