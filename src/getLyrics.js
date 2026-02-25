32dc9c1cea9634775d973bf9de0bf24b4c1f3250
Samin Ahmed <saminahmed1001@gmail.com>
2026-02-26 05:03:45 +0600

    Refactor: move lyrics fetching to dedicated file

---
+++ src/getLyrics.js
@@ -0,0 +1,38 @@
+import extractInfo from "./extractInfo.js";
+import fetchLyrics from "../public/background.js";
+
+export default async function getLyrics(
+  setLyrics,
+  setFetched,
+  manuallyTypedInfo = null,
+) {
+  setFetched("fetching");
+
+  const songInfo = manuallyTypedInfo ? manuallyTypedInfo : extractInfo();
+
+  if (songInfo === "No_metadata") {
+    setFetched("No_metadata");
+  }
+
+  const response = await fetchLyrics(songInfo);
+
+  if (response === "couldn't_fetch") {
+    setFetched("couldn't_fetch");
+  } else {
+    filterLyrics(response);
+    setFetched("fetched_successfully");
+  }
+
+  const fileredResponse = filterLyrics(response);
+
+  setLyrics({
+    fetchedLyrics: fileredResponse,
+    currentlySelectedLyrics: 0,
+    lyricsCount: fileredResponse.length,
+  });
+}
+
+// Fiters the empty string, random Portugese strings and duplicates
+function filterLyrics(arr) {
+  return arr;
+}
