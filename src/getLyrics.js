6320f409875ea2552def309cb1ada52f785b2caf
Samin Ahmed <saminahmed1001@gmail.com>
2026-03-01 19:19:55 +0600

    Feat: add manually typed info in DB

--- src/getLyrics.js
+++ src/getLyrics.js
@@ -1,24 +1,59 @@
 import extractInfo from "./extractInfo.js";
 import fetchLyrics from "../public/background.js";
 
+import getDB from "./indexedDB.js";
+
 export default async function getLyrics(
   setLyrics,
-  setFetched,
-  manuallyTypedInfo = null,
+  status,
+  setStatus,
+  videoInfo,
 ) {
-  setFetched("fetching");
-
-  const songInfo = manuallyTypedInfo ? manuallyTypedInfo : extractInfo();
-
-  if (songInfo === "No_metadata") {
-    setFetched("No_metadata");
+  // this function should not be running if the status is not set to "fetching",
+  // but due to the useEffect in the AppContent, it will get called everytime the
+  // component gets re-rendered, as it only gets re-rendered when status updates and it's an dependency.
+  if (status !== "fetching") {
     return;
   }
 
+  const metadata = !!document.querySelector(
+    ".yt-video-attribute-view-model__metadata",
+  );
+
+  if (!metadata) {
+    setStatus("manual_search");
+    return;
+  }
+
+  const isManuallyTyped = videoInfo.manuallyTyped;
+
+  let songInfo;
+
+  const db = getDB();
+  const record = await db.videos.get(videoInfo.videoID);
+
+  if (record) {
+    songInfo = {
+      songTitle: record.songTitle,
+      artistName: record.artistName,
+    };
+  } else {
+    songInfo = isManuallyTyped
+      ? { songTitle: videoInfo.songTitle, artistName: videoInfo.artistName }
+      : extractInfo();
+  }
+
+  // When the manual searching button gets clicked, it will firstly go through the normal fetching process,
+  // but if it can't find the metadata or an entry in db(which is checked before this if statement),
+  // it will trigger the manual search
+
+  console.log(songInfo);
   const response = await fetchLyrics(songInfo);
 
-  if (response === "couldn't_fetch") {
-    setFetched("couldn't_fetch");
+  // When the fetching process fails, due to the metadata having junk it in, or not having a metadata at all,
+  // it will call manual search, this block will also call manual search if the user given info is incorrect
+  if (response === "failed") {
+    setStatus("manual_search");
     return;
   }
 
@@ -30,10 +65,20 @@ export default async function getLyrics(
     lyricsCount: fileredResponse?.length ?? 0,
   });
 
-  setFetched("fetched_successfully");
+  if (isManuallyTyped) {
+    const data = {
+      videoID: videoInfo.videoID,
+      songTitle: videoInfo.songTitle,
+      artistName: videoInfo.artistName,
+    };
+
+    db.videos.put(data);
+  }
+
+  setStatus("mount");
 }
 
-// Fiters the empty string, random Portugese strings and duplicates
+// Fiters the empty string, random cursed Portugese strings and duplicates
 function filterLyrics(arr) {
   const lyricArr = [];
   arr.forEach((obj) => {
