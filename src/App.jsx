957fa6debdd65ebd1bb0fd4cfdccceb26c513df2
Samin Ahmed <saminahmed1001@gmail.com>
2026-03-01 19:26:32 +0600

    Refactor: remove mount state and use status state instead and add comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,84 @@
 import { useState, useEffect } from "react";
 import { AppContext } from "./context";
-
+// import { useLiveQuery } from "dexie-react-hooks";
 import AppContent from "./Components/AppContent/AppContent";
+import getDB from "./indexedDB";
+import extractInfo from "./extractInfo";
+
+// "fetching", Will trigger loading screen, when:
+// 1. If metadata exists
+// 2. If metadata does not exists BUT DB entry exists
+// 3. After manually searched
+
+// "manual_search", will trigger manual search form, when:
+// 1. when metadata exists but failed to fetch
+// 2. when the button is clicked but no metadata or db entry was found
+
+// "mount", will trigger rendering the app when:
+//  1.  Lyrics has been fetched sucessfully
+
+// "unmount", will trigger when div is not supposed to render:
+// 1. No metadata exists
+// 2. When the close button was pressed
 
 export default function App() {
   const metadata = !!document.querySelector(
     ".yt-video-attribute-view-model__metadata",
   );
 
-  const [mount, setMount] = useState(metadata);
+  const { songTitle, artistName } = extractInfo();
+
+  const ytFlexy = document.querySelector("ytd-watch-flexy");
+  const getVideoID = ytFlexy.getAttribute("video-id");
+
   const [videoInfo, setVideoInfo] = useState({
-    videoID: null,
-    attributed: null,
-    songTitle: null,
-    artistName: null,
+    manuallyTyped: false,
+    videoID: getVideoID,
+    attributed: metadata,
+    songTitle: songTitle,
+    artistName: artistName,
   });
 
-  const [status, setStatus] = useState("fetching");
+  const [status, setStatus] = useState(metadata ? "fetching" : "unmount");
 
   const [lyrics, setLyrics] = useState({
     fetchedLyrics: [],
     currentlySelectedLyrics: 0,
     lyricsCount: 0,
   });
+
   const [fontSize, setFontSize] = useState(1.4);
 
+  // this block is responsible for two tasks,
+  // 1. check for the db entry on inital run
+  // 2. check for the db entry on demand(when button gets clicked), if no entry, trigger manual search
   useEffect(() => {
+    const run = async () => {
+      const db = getDB();
+      const record = await db.videos.get(getVideoID);
+      if (record) {
+        console.log("IN app.jsx use effect");
+        setStatus("fetching");
+      }
+    };
+
+    run();
+
     const ytpControls = document.querySelector(".ytp-right-controls-left");
 
-    const handler = (e) => {
+    const handler = async (e) => {
+      const appDivMount = !!document.querySelector(".appDiv");
+      if (appDivMount) {
+        return;
+      }
+
       const btn = e.target.closest(".manualSearchTriggerBtn");
       if (!btn) return;
 
-      setMount(true);
+      const db = getDB();
+      const record = await db.videos.get(getVideoID);
+
+      setStatus(record || metadata ? "fetching" : "manual_search");
     };
 
     ytpControls.addEventListener("click", handler);
@@ -40,15 +86,13 @@ export default function App() {
     return () => {
       ytpControls.removeEventListener("click", handler);
     };
-  }, [mount]);
+  }, [getVideoID, metadata]);
 
   const app = () => {
     return (
       <div className="appDiv">
         <AppContext.Provider
           value={{
-            mount,
-            setMount,
             videoInfo,
             setVideoInfo,
             status,
@@ -65,5 +109,5 @@ export default function App() {
     );
   };
 
-  return mount ? app() : null;
+  return status === "unmount" ? null : app();
 }
