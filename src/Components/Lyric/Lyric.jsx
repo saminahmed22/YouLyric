8e1b1b9be3ef41ac15ad8d5790cf5092d96e9813
Samin Ahmed <saminahmed1001@gmail.com>
2026-03-05 02:55:35 +0600

    Refactor: set lyricsText pre tag height based on player size on YouTube

--- src/Components/Lyric/Lyric.jsx
+++ src/Components/Lyric/Lyric.jsx
@@ -1,37 +1,73 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AppContext } from "../../context";
 
 import styles from "./Lyric.module.css";
 
 export default function Lyric() {
-  const { lyrics, fontSize } = useContext(AppContext);
+  const { lyrics, fontSize, settings } = useContext(AppContext);
 
   const currentLyricsIndex = lyrics.currentlySelectedLyrics;
   const currentLyrics = lyrics.fetchedLyrics[currentLyricsIndex];
+
+  useEffect(() => {
+    const currentDock = settings.currentDock;
+    const lyricsText = document.querySelector(".lyricsTextPre");
+    const video = document.querySelector("video");
+    const height = video.clientHeight;
+    const textMaxHeight = height - 149;
+
+    if (currentDock === "sidebar") {
+      lyricsText.style.setProperty(
+        "max-height",
+        `${textMaxHeight}px`,
+        "important",
+      );
+    }
+  }, [settings.currentDock, settings.pipSize]);
 
   return (
     <div className={styles.contentDiv}>
       <div className={styles.lyricContainer}>
         <pre
-          className={styles.lyricText}
+          className={`${settings.currentDock === "PIP" ? styles.lyricsTextPIP : styles.lyricsText}  lyricsTextPre`}
           style={{
             fontSize: `${fontSize}rem`,
+            maxHeight: settings.pipSize.height - 149,
           }}
         >
           {currentLyrics}
         </pre>
-        <p className={styles.credit}>
-          Lyrics were fetched from
-          <a
-            href="https://lrclib.net/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+        <div className={styles.credit}>
+          <p className={styles.creditText}>
+            Lyrics were fetched from{" "}
+            <a
+              href="https://lrclib.net/"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{
+                color: settings.fontColor,
+              }}
+            >
+              LRCLIB
+            </a>
+            .
+          </p>
+          <p className={styles.creditText}>
             {" "}
-            LRCLIB
-          </a>
-          . Please consider giving them a donation.
-        </p>
+            Please consider giving them a{" "}
+            <a
+              href="https://github.com/tranxuanthang/lrclib#donation"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{
+                color: settings.fontColor,
+              }}
+            >
+              donation
+            </a>
+            .
+          </p>
+        </div>
       </div>
     </div>
   );
