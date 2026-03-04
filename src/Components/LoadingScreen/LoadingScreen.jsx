9ddd9e9bc3647221a91ed54b5afa8ed7f25e5574
Samin Ahmed <saminahmed1001@gmail.com>
2026-03-05 02:49:55 +0600

    Refactor: remove close button

--- src/Components/LoadingScreen/LoadingScreen.jsx
+++ src/Components/LoadingScreen/LoadingScreen.jsx
@@ -1,26 +1,11 @@
-import { useContext } from "react";
-import { AppContext } from "../../context";
-
 import loadingIcon from "../../../icons/loading.png";
-import closeIcon from "../../../icons/close.png";
 
 import styles from "./LoadingScreen.module.css";
 
 export default function LoadingScreen() {
-  const { setStatus } = useContext(AppContext);
-
   const loadingScreenContent = () => {
     return (
       <div className={styles.LoadingScreenDiv}>
-        <button
-          className={`${styles.closeBtn} extensionBtn`}
-          title="Close"
-          onClick={() => {
-            setStatus("unmount");
-          }}
-        >
-          <img src={closeIcon} alt="Close icon" />
-        </button>
         <div className={styles.loadingMessage}>
           <img
             className={styles.loadingIcon}
