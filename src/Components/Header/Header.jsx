0f13c9dd247e8fd9c5a0f24db953c041fb329639
Samin Ahmed <saminahmed1001@gmail.com>
2026-03-05 02:52:33 +0600

    Feat: replace settings icon with lyrics icon on click, make icons non-draggable

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -9,46 +9,59 @@ import rightIcon from "../../../icons/chevronRight.png";
 import leftIcon from "../../../icons/chevronLeft.png";
 
 export default function Header() {
-  const { lyrics, setLyrics } = useContext(AppContext);
+  const { status, lyrics, setLyrics } = useContext(AppContext);
 
   const lyricsQuantity = lyrics.lyricsCount;
   const currentLyricIndex = lyrics.currentlySelectedLyrics;
 
   return (
     <div className={styles.header}>
-      <div className={styles.lyricTitle}>
-        Lyrics
-        <div className={styles.lyricScroll}>
-          <button
-            className={`${styles.scrollBtn} extensionBtn`}
-            title="Scroll left"
-            onClick={() => {
-              setLyrics({
-                ...lyrics,
-                currentlySelectedLyrics: currentLyricIndex - 1,
-              });
-            }}
-            disabled={currentLyricIndex + 1 <= 1}
-          >
-            <img src={leftIcon} alt="Left icon" />
-          </button>
-          <span>
-            {currentLyricIndex + 1}/{lyricsQuantity}
-          </span>
-          <button
-            className={`${styles.scrollBtn} extensionBtn`}
-            title="Scroll right"
-            onClick={() => {
-              setLyrics({
-                ...lyrics,
-                currentlySelectedLyrics: currentLyricIndex + 1,
-              });
-            }}
-            disabled={currentLyricIndex + 1 >= lyricsQuantity}
-          >
-            <img src={rightIcon} alt="Right icon" />
-          </button>
-        </div>
+      <div className={styles.headerTitle}>
+        <span className={styles.headerTitleText} draggable={false}>
+          {status === "mount"
+            ? "Lyrics"
+            : status === "options"
+              ? "Options"
+              : ""}
+        </span>
+        {status === "mount" && (
+          <div className={styles.lyricScroll}>
+            <button
+              className={`${styles.scrollBtn} extensionBtn`}
+              title="Scroll left"
+              onClick={() => {
+                setLyrics({
+                  ...lyrics,
+                  currentlySelectedLyrics: currentLyricIndex - 1,
+                });
+              }}
+              disabled={currentLyricIndex + 1 <= 1}
+              draggable={false}
+            >
+              <img src={leftIcon} alt="Left icon" draggable={false} />
+            </button>
+            <span
+              className={styles.lyricsVariationText}
+              title="Lyrics variations"
+            >
+              {currentLyricIndex + 1}/{lyricsQuantity}
+            </span>
+            <button
+              className={`${styles.scrollBtn} extensionBtn`}
+              title="Scroll right"
+              onClick={() => {
+                setLyrics({
+                  ...lyrics,
+                  currentlySelectedLyrics: currentLyricIndex + 1,
+                });
+              }}
+              disabled={currentLyricIndex + 1 >= lyricsQuantity}
+              draggable={false}
+            >
+              <img src={rightIcon} alt="Right icon" draggable={false} />
+            </button>
+          </div>
+        )}
       </div>
       <ActDiv />
     </div>
